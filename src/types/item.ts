ce2d02b48536aa4548f9f875c9fb91b3b0eab761
pg805 <macattack.carr@gmail.com>
2023-07-03 16:51:25 -0700

    added to and from json functions

--- src/types/item.ts
+++ src/types/item.ts
@@ -1,4 +1,10 @@
-import Action from "./action";
+import Action, {ActionObject} from "./action";
+
+export interface ItemObject {
+    hp: number,
+    name: string,
+    actions: ActionObject[];
+}
 
 export default class Item {
     constructor(
@@ -6,4 +12,22 @@ export default class Item {
         public name: string = '',
         public actions: Action[] = []
     ) { }
+
+    toJSON(): ItemObject {
+        return {
+            hp: this.hp,
+            name: this.name,
+            actions: this.actions.map((action: Action) => action.toJSON())
+        }
+    }
+
+    static fromJSON(itemObject: ItemObject): Item {
+        const item = new Item()
+
+        item.hp = itemObject.hp;
+        item.name = itemObject.name;
+        item.actions = itemObject.actions.map((actionObject: ActionObject) => Action.fromJSON(actionObject))
+
+        return item
+    }
 }
