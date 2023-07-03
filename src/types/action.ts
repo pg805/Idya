1286a5a81a27a8b8623ab2f2dcc85e7e8dc00c0c
pg805 <macattack.carr@gmail.com>
2023-07-03 16:46:05 -0700

    added to and from json functions

--- src/types/action.ts
+++ src/types/action.ts
@@ -1,5 +1,13 @@
-import Effect from "./effect";
-import ActionType from "./actionType";
+import Effect, { EffectObject } from "./effect";
+import ActionType, { ACTION_TYPES } from "./actionType";
+
+interface ActionObject {
+    name: string,
+    actionType: string,
+    effect: EffectObject,
+    targetId: string,
+    originId: string
+}
 
 export default class Action {
 
@@ -11,4 +19,34 @@ export default class Action {
         public originId: string = ''
     ) { }
 
+    toJSON(): ActionObject {
+        return {
+            name: this.name,
+            actionType: this.actionType.type,
+            effect: this.effect.toJSON(),
+            targetId: this.targetId,
+            originId: this.originId
+        }
+    }
+
+    static fromJSON(actionObject: ActionObject): Action {
+        const action = new Action();
+        const actionType = new ActionType();
+
+        if (actionObject.actionType == ACTION_TYPES.ATTACK) {
+            actionType.setAttack()
+        } else if (actionObject.actionType == ACTION_TYPES.DEFEND) {
+            actionType.setDefend()
+        } else if (actionObject.actionType == ACTION_TYPES.SPECIAL) {
+            actionType.setSpecial()
+        }
+
+        action.actionType = actionType;
+        action.effect = Effect.fromJSON(actionObject.effect);
+        action.name = actionObject.name;
+        action.originId = actionObject.originId;
+        action.targetId = actionObject.targetId;
+        
+        return action;
+    }
 }
