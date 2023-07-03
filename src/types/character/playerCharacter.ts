c6d4db594bb8fdd48f54bb97f88f22ce8ed1e032
pg805 <macattack.carr@gmail.com>
2023-07-03 16:58:51 -0700

    added to json and from json functions

--- src/types/character/playerCharacter.ts
+++ src/types/character/playerCharacter.ts
@@ -1,7 +1,44 @@
 import Character from "../character";
+import Item, { ItemObject } from "../item";
+
+export interface PlayerCharacterObject {
+    userId: string,
+    id: string,
+    totalHP: number,
+    currentHP: number,
+    armor: number,
+    inventory: ItemObject[]
+    activeItems: ItemObject[]
+}
 
 export default class PlayerCharacter extends Character {
     constructor(
         public userId: string = "",
     ) { super() }
+
+    toJSON(): PlayerCharacterObject {
+        return {
+            id: this.id,
+            userId: this.userId,
+            totalHP: this.totalHp,
+            currentHP: this.currentHp,
+            armor: this.armor,
+            inventory: this.inventory.map((item: Item) => item.toJSON()),
+            activeItems: this.activeItems.map((item: Item) => item.toJSON())
+        }
+    }
+
+    static fromJSON(playerCharacterObject: PlayerCharacterObject): PlayerCharacter {
+        const playerCharacter = new PlayerCharacter()
+
+        playerCharacter.id = playerCharacterObject.id;
+        playerCharacter.userId = playerCharacterObject.userId;
+        playerCharacter.totalHp = playerCharacterObject.totalHP;
+        playerCharacter.currentHp = playerCharacterObject.currentHP;
+        playerCharacter.armor = playerCharacterObject.armor;
+        playerCharacter.inventory = playerCharacterObject.inventory.map((itemObject: ItemObject) => Item.fromJSON(itemObject));
+        playerCharacter.activeItems = playerCharacterObject.activeItems.map((itemObject: ItemObject) => Item.fromJSON(itemObject));
+
+        return playerCharacter
+    }
 }
