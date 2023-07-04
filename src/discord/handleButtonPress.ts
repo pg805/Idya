9a894eb6380a43779eeff33ec78dfbf92ce18c35
pg805 <macattack.carr@gmail.com>
2023-07-03 19:34:08 -0700

    added break case

--- src/discord/handleButtonPress.ts
+++ src/discord/handleButtonPress.ts
@@ -5,7 +5,7 @@ import { ButtonInteraction } from 'discord.js'
 export function handleButtonPress(interaction: ButtonInteraction) {
     switch(interaction.customId) {
         case interaction.customId.match(/^battle\s.+/)?.input:
-
+            break;
         default:
             logger.error("Unkown Button Pressed");
             logger.error(`Channel - ${interaction.channelId}`);
