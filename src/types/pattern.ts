35ee85a9ea9594020ce69abe17fd09955eddd90b
pg805 <macattack.carr@gmail.com>
2023-07-03 17:12:41 -0700

    added to json and from json functions

--- src/types/pattern.ts
+++ src/types/pattern.ts
@@ -1,6 +1,27 @@
+export interface PatternObject {
+    sequence: number[],
+    deviationPercent: number
+}
+
 export default class Pattern {
     constructor (
         public sequence: number[] = [],
         public deviationPercent: number = 0
     ) {}
+
+    toJSON(): PatternObject {
+        return {
+            sequence: this.sequence,
+            deviationPercent: this.deviationPercent
+        }
+    }
+
+    static fromJSON(patternObject: PatternObject): Pattern {
+        const pattern = new Pattern()
+
+        pattern.sequence = patternObject.sequence;
+        pattern.deviationPercent = patternObject.deviationPercent;
+
+        return pattern
+    }
 }
