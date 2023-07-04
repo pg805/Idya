a2f95fe18f77eed240026696717ad88d6f41821d
pg805 <macattack.carr@gmail.com>
2023-07-03 17:40:11 -0700

    added to and from json functions

--- src/_store/state.ts
+++ src/_store/state.ts
@@ -1,12 +1,24 @@
-import NonPlayerCharacter from "../types/character/nonPlayerCharacter"
-import PlayerCharacter from "../types/character/playerCharacter"
-import Item from "../types/item"
-import Action from "../types/action"
-import Character from "../types/character"
+import NonPlayerCharacter, { NonPlayerCharacterObject } from "../types/character/nonPlayerCharacter"
+import PlayerCharacter, { PlayerCharacterObject } from "../types/character/playerCharacter"
+import Item, { ItemObject } from "../types/item"
+import Action, { ActionObject } from "../types/action"
+import Character, { CharacterObject } from "../types/character"
+import { v4 as uuidv4 } from 'uuid';
+
+export interface StateObject {
+    id: string,
+    playerCharacters: PlayerCharacterObject[],
+    nonPlayerCharacters: NonPlayerCharacterObject[],
+    deadCharacters: (PlayerCharacterObject|NonPlayerCharacterObject|CharacterObject)[],
+    definedIntents: ActionObject[],
+    round: number,
+    loot: ItemObject[]
+}
 
 export default class State {
 
     constructor(
+        public id: string = '',
         public playerCharacters: PlayerCharacter[] = [],
         public nonPlayerCharacters: NonPlayerCharacter[] = [],
         public deadCharacters: Character[] = [],
@@ -15,12 +27,52 @@ export default class State {
         public loot: Item[] = [],
     ) { }
 
-}
+    toJSON(): StateObject {
+        return {
+            id: this.id,
+            round: this.round,
+            playerCharacters: this.playerCharacters.map((pc: PlayerCharacter) => pc.toJSON()),
+            nonPlayerCharacters: this.nonPlayerCharacters.map((npc: NonPlayerCharacter) => npc.toJSON()),
+            deadCharacters: this.deadCharacters.map((character: Character) => {
+                if (Character.isPC(character)) {
+                    return character.toJSON()
+                } else if (Character.isNPC(character)) {
+                    return character.toJSON()
+                } else {
+                    return character.toJSON()
+                }
+            }),
+            definedIntents: this.definedIntents.map((action: Action) => action.toJSON()),
+            loot: this.loot.map((item: Item) => item.toJSON())
+        }
+    }
 
-export let state: State;
+    static fromJSON(stateObject: StateObject): State {
+        const state: State = new State();
+
+        state.id = stateObject.id;
+        state.round = stateObject.round;
+        state.definedIntents = stateObject.definedIntents.map((actionObject: ActionObject) => Action.fromJSON(actionObject));
+        state.playerCharacters = stateObject.playerCharacters.map((playerCharacterObject: PlayerCharacterObject) => PlayerCharacter.fromJSON(playerCharacterObject));
+        state.nonPlayerCharacters = stateObject.nonPlayerCharacters.map((nonPlayerCharacterObject: NonPlayerCharacterObject) => NonPlayerCharacter.fromJSON(nonPlayerCharacterObject));
+        state.deadCharacters = stateObject.deadCharacters.map((character: (PlayerCharacterObject | NonPlayerCharacterObject | CharacterObject)) => {
+            if (Character.isPCObject(character)) {
+                return PlayerCharacter.fromJSON(character)
+            } else if (Character.isNPCObject(character)) {
+                return NonPlayerCharacter.fromJSON(character)
+            } else {
+                return Character.fromJSON(character)
+            }
+        })
+
+        return state
+    }
+
+}
 
 // gets called from init.ts
 export const initializeState = () => {
-    state = new State();
+    const state = new State();
+    state.id = uuidv4();
     // sync up with backend
 }
