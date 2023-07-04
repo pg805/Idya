36165d476a1ea16b3540c796509c3a242c8385d2
pg805 <macattack.carr@gmail.com>
2023-07-04 16:13:20 -0700

    added logginng

--- src/discord/handleButtonPress.ts
+++ src/discord/handleButtonPress.ts
@@ -1,10 +1,18 @@
 // library dependencies
+import { handleBattleButton } from '../combat/runBattle.js';
 import logger from '../util/logger.js';
 import { ButtonInteraction } from 'discord.js'
 
 export function handleButtonPress(interaction: ButtonInteraction) {
+    logger.debug(`
+Handling Button
+Custom ID: ${interaction.customId}
+Guild ID: ${interaction.guildId}
+Channel ID: ${interaction.channelId}
+`)
     switch(interaction.customId) {
         case interaction.customId.match(/^battle\s.+/)?.input:
+            handleBattleButton(interaction)
             break;
         default:
             logger.error("Unkown Button Pressed");
