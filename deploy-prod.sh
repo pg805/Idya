7b5255c84b80b9c83e48033796c2ba7e5c902c3f
pg805 <macattack.carr@gmail.com>
2026-05-28 22:26:22 -0700

    make deploy scripts resilient: fetch+reset instead of pull

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,7 +3,8 @@ set -e
 exec 9>/tmp/idya-prod-deploy.lock
 flock 9
 cd /home/mac-admin/Idya-prod
-git pull origin main
+git fetch origin main
+git reset --hard origin/main
 npm install --include=dev
 npx prisma migrate deploy
 npx prisma generate
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ set -e
 exec 9>/tmp/idya-dev-deploy.lock
 flock 9
 cd /home/mac-admin/Idya
-git pull origin dev
+git fetch origin dev
+git reset --hard origin/dev
 npm install --include=dev
 npx prisma migrate deploy
 npx prisma generate
